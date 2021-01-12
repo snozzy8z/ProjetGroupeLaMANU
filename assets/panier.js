c9934de636eb738dd4042ab8f86a491f0a400d5a
Snozzy8z <atoxmillenium@gmail.com>
2021-01-12 16:21:01 +0100

    ajout fonction panier

---
+++ assets/panier.js
@@ -0,0 +1,61 @@
+var carts = document.querySelectorAll('.card');
+
+var product = [
+    {
+        name : 'Alimentation corsair',
+        tag : 'alimcorsair',
+        price : '30',
+        inCart: 0
+    },
+    {
+        name : 'Alimentation be quiet',
+        tag : 'alimbe quiet',
+        price : '30',
+        inCart: 0
+    },
+]
+
+for (i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(product[i]);
+    })
+}
+
+function onLoadCartNumbers() {
+    var productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers){
+        document.querySelector('.panier span').textContent = productNumbers;
+
+    }
+
+}
+
+function cartNumbers(product) {
+    var productNumbers = localStorage.getItem('cartNumbers');
+
+
+    productNumbers = parseInt(productNumbers)
+
+    if (productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.panier span').textContent = productNumbers + 1;
+
+    }else {
+        localStorage.setItem('cartNumbers', 1)
+        document.querySelector('.panier span').textContent = 1;
+    }
+    setItems(product)
+}
+
+function setItems(product){
+    console.log("inside of set items fucntion")
+    console.log("my product is", product)
+    product.inCart = 1;
+    var cartItems = {
+        [product.tag]: product
+    }
+    localStorage.setItem("productsInCart", cartItems )
+}
+
+onLoadCartNumbers();
