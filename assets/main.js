072c477b38b42fc6c417e762b46360bc8d1cec83
cmontillot <cedric.montillot@wanadoo.fr>
2021-01-12 09:04:58 +0100

    Modif fichier javascript - fonction log in

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,14 @@
-var admin = [{
-    username: "jérémy",
-    password: "admin"
-},
-{
-    username: "cédric",
-    password: "admin"
-},
-{
-    username: "tony",
-    password: "admin"
-}]
+var log = document.getElementById('login');
 
-
-function getInfo() {
-    var username = document.getElementById('name').value
-    var password = document.getElementById('password').value
-
-    for (i = 0; i < admin.length; i++) {
-        if (username == admin[i].username && password == admin[i].password) {
-            alert(username + " is logged in !!!")
-        } else {
-            alert("incorrect username or password ! Sign up !")
-        }
-    }
-}
+log.onclick = function() {
+    var username = document.getElementById('name').value;
+    var password = document.getElementById('password').value;
+    let password1 ='admin';
+    let username1 = 'jérémy';
+    let username2 = 'cédric';
+    let username3 = 'tony';
+    if (password == password1 && (username == username1 || username == username2 || username == username3))
+        alert('Bienvenue sur YouPorn ' + username);
+    else 
+        alert('Identifiants de connexion incorrects !');
+};
