8c3c5bc9973f00b41f94cea5018b648fa1bafa97
arshess <s3th80@gmail.com>
2021-01-13 11:33:25 +0100

    ajout id sur la card : "article"+idArticle

--- assets/script.js
+++ assets/script.js
@@ -8,32 +8,32 @@ for (const key in dropdownmenu) {
             switch (element.id){
                 case 'menu-alim':
                     document.getElementById("menugauche-alim").style.display="block";
-                    document.getElementById("menugauche-disquedur").style.display ="none"
-                    document.getElementById("menugauche-proco").style.display ="none"
-                    document.getElementById("menugauche-ram").style.display ="none"
+                    document.getElementById("menugauche-disquedur").style.display ="none";
+                    document.getElementById("menugauche-proco").style.display ="none";
+                    document.getElementById("menugauche-ram").style.display ="none";
                     break;
                 case "menu-disquedur":
                     document.getElementById("menugauche-alim").style.display="none";
-                    document.getElementById("menugauche-disquedur").style.display ="block"
-                    document.getElementById("menugauche-proco").style.display ="none"
-                    document.getElementById("menugauche-ram").style.display ="none"
+                    document.getElementById("menugauche-disquedur").style.display ="block";
+                    document.getElementById("menugauche-proco").style.display ="none";
+                    document.getElementById("menugauche-ram").style.display ="none";
                     break;
                 case "menu-processeur":
                     document.getElementById("menugauche-alim").style.display="none";
-                    document.getElementById("menugauche-disquedur").style.display ="none"
-                    document.getElementById("menugauche-proco").style.display ="block"
-                    document.getElementById("menugauche-ram").style.display ="none"
+                    document.getElementById("menugauche-disquedur").style.display ="none";
+                    document.getElementById("menugauche-proco").style.display ="block";
+                    document.getElementById("menugauche-ram").style.display ="none";
                     break;
                 case "menu-ram":
                     document.getElementById("menugauche-alim").style.display="none";
-                    document.getElementById("menugauche-disquedur").style.display ="none"
-                    document.getElementById("menugauche-proco").style.display ="none"
-                    document.getElementById("menugauche-ram").style.display ="block"
+                    document.getElementById("menugauche-disquedur").style.display ="none";
+                    document.getElementById("menugauche-proco").style.display ="none";
+                    document.getElementById("menugauche-ram").style.display ="block";
                     break;  
                 default:
                     break;
             }
-        })
+        });
     }
 }
 var test = document.getElementById("puissance-alim")
@@ -52,14 +52,15 @@ fetch("./assets/listearticle.json")
         var divcont = document.createElement("div");
         var divleft = document.createElement("div");
         var img = document.createElement("img");
-        var divmid =  document.createElement("div");
-        var titre =  document.createElement("h4");
-        var lientitre =  document.createElement("a");
+        var divmid = document.createElement("div");
+        var titre = document.createElement("h4");
+        var lientitre = document.createElement("a");
         var descr =  document.createElement("p");
-        var divright =  document.createElement("div");
-        var titreprix =  document.createElement("p");
-        var prix =  document.createElement("p");
+        var divright = document.createElement("div");
+        var titreprix = document.createElement("p");
+        var prix = document.createElement("p");
         var button = document.createElement("button");
+        card.setAttribute("id", "article"+article.idArticle);
         card.setAttribute("class", "card m-2 col-12 border-0");
         divcont.setAttribute("class", "row no-gutters");
         divleft.setAttribute("class", "col-md-3");
@@ -72,7 +73,7 @@ fetch("./assets/listearticle.json")
         titreprix.setAttribute("class", "card-title");
         prix.setAttribute("class", "card-text font-weight-bold");
         button.setAttribute("class", "btn btn-primary mt-5");
-        img.src= "./assets/images/"+ article.imageArticle;
+        img.src = "./assets/images/"+ article.imageArticle;
         button.textContent = "Acheter";
         descr.textContent = article.descArticle;
         lientitre.href = "./detail_article.html?id="+article.idArticle;
@@ -93,9 +94,6 @@ fetch("./assets/listearticle.json")
         card.appendChild(divcont);
         //on affiche l'article à la suite des précédents
         main.appendChild(card);
-        console.log(card);
     }
-
-
 });
 });
