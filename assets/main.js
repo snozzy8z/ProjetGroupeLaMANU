7f344549246eb0d99cb5cd774a3f54988a5cd26d
cmontillot <cedric.montillot@wanadoo.fr>
2021-01-13 11:03:29 +0100

    Modif page contact et inscription

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,9 @@ var valid_form = document.getElementById('valid_form');
 // Bouton Inscription de la Newsletter
 var button_mail = document.getElementById('button_mail');
 
+// Bouton Inscription au site
+var valid_inscript = document.getElementById('valid_inscript');
+
 // Contrôle des identifiants quand le user cliquer sur le bouton Login de la page Connexion
 if (log != undefined) {
     log.onclick = function () {
@@ -26,26 +29,75 @@ if (log != undefined) {
 // Contrôle des champs saisis sur le formulaire de Contact
 if (valid_form != undefined) {
     valid_form.onclick = function () {
-        var last = document.getElementById('lastname').value
-        var first = document.getElementById('firstname').value;
-        var adr_mail = document.getElementById('adr_mail').value;
-        var adr_post = document.getElementById('adr_post').value;
-        var mess = document.getElementById('mess_info');
-        if (last == '') { alert('Veuillez saisir votre nom.'); return false; }
-        if (first == '') { alert('Veuillez saisir votre prénom.'); return false; }
-        if (adr_mail == '') { alert('Veuillez saisir votre adresse mail.'); return false; }
-        if (adr_post == '') { alert('Veuillez saisir votre adresse postale.'); return false; }
-        if (mess == '') { alert('Veuillez saisir votre message.'); return false; }
-        mess.innerHTML = '<div class="alert alert-success">Merci pour votre message. Nous y répondrons sous 48h.</div>';
+        var last = document.getElementById('lastname');
+        var first = document.getElementById('firstname');
+        var adr_post = document.getElementById('adr_post');
+        var mess = document.getElementById('mess');
+        var mess_info = document.getElementById('mess_info');
+        var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+        var adr_mail = document.getElementById("adr_mail");
+        if (last.value == '') { alert('Veuillez saisir votre nom.'); return false; }
+        if (first.value == '') { alert('Veuillez saisir votre prénom.'); return false; }
+        if (!regexEmail.test(adr_mail.value)) { alert("Veuillez saisir une adresse mail valide."); return false; }
+        if (adr_post.value == '') { alert('Veuillez saisir votre adresse postale.'); return false; }
+        if (mess.value == '') { alert('Veuillez saisir votre message.'); return false; }
+        mess_info.innerHTML = '<div class="alert alert-success">Merci pour votre message. Nous y répondrons sous 48h.</div>';
+        last.value = '';
+        first.value = '';
+        adr_post.value = '';
+        adr_mail.value = '';
+        mess.value = '';
+        return false;
+    };
+};
+
+// Contrôle des champs saisis sur le formulaire d'inscription'
+if (valid_inscript != undefined) {
+    valid_inscript.onclick = function () {
+        var last = document.getElementById('lastname');
+        var first = document.getElementById('firstname');
+        var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+        var adr_mail = document.getElementById("adr_mail");
+        var pass = document.getElementById('password');
+        var pass2 = document.getElementById('password2');
+        var mess_info = document.getElementById('mess_info');
+        if (last.value == '') { alert('Veuillez saisir votre nom.'); return false; }
+        if (first.value == '') { alert('Veuillez saisir votre prénom.'); return false; }
+        if (!regexEmail.test(adr_mail.value)) { alert("Veuillez saisir une adresse mail valide."); return false; }
+        if (pass.value == '') { alert('Veuillez saisir votre mot de passe.'); return false; }
+        if (pass2.value == '') { alert('Veuillez confirmer votre mot de passe.'); return false; }
+        if (pass.value != pass2.value) { alert('Echec de confirmation du mot de passe.'); return false; }
+        mess_info.innerHTML = '<div class="alert alert-success">Un mail vient de vous être envoyé afin de valider votre adresse mail et terminer votre inscription.</div>';
+        last.value = '';
+        first.value = '';
+        adr_mail.value = '';
+        pass.value = '';
+        pass2.value = '';
+        return false;
     };
 };
 
 // Contrôle de l'adresse mail pour inscription à la Newsletter
 if (button_mail != undefined) {
     button_mail.onclick = function () {
-        var news_mail = document.getElementById('email').value;
-        if (news_mail == '') { alert('Veuillez saisir votre adresse mail.'); return false; }
-        else { alert('Merci'); return true; }
+        var news_mail = document.getElementById('email');
+        var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+        if (!regexEmail.test(news_mail.value)) { alert('Veuillez saisir une adresse mail valide.'); return false; }
+        else { 
+            alert('Votre inscription à notre Newsletter a bien été prise en compte.');
+            return true; 
+        }
     };
+};
+
+// Contrôle du format d'une adresse mail
+function ValidateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
 };
 
