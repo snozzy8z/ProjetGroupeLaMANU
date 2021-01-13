b0a584d88f1b55f8565c99adf971d4ff10221b8c
arshess <s3th80@gmail.com>
2021-01-13 10:47:39 +0100

    utilisation Json pour remplir page (c'est trop classe !)

--- assets/script.js
+++ assets/script.js
@@ -41,33 +41,58 @@ test.addEventListener("change", function(){
     document.getElementById("puissancealim").innerHTML = test.value;
 })
 //chargement Json pour remplir page
-fetch("./assets/listarticle.json").then(
-    response =>{
-        console.log(response);
+var main = document.querySelector('main');
+fetch("./assets/listearticle.json")
+.then(response => response.json())
+.then(data => {
+    for (const article of data) {
+        // console.log(article);
+        //on crée l'architecture de la card article
+        var card = document.createElement("article");
+        var divcont = document.createElement("div");
+        var divleft = document.createElement("div");
+        var img = document.createElement("img");
+        var divmid =  document.createElement("div");
+        var titre =  document.createElement("h4");
+        var lientitre =  document.createElement("a");
+        var descr =  document.createElement("p");
+        var divright =  document.createElement("div");
+        var titreprix =  document.createElement("p");
+        var prix =  document.createElement("p");
+        card.setAttribute("class", "card m-2 col-12 border-0");
+        divcont.setAttribute("class", "row no-gutters");
+        divleft.setAttribute("class", "col-md-3");
+        img.setAttribute("class", "card-img");
+        divmid.setAttribute("class", "card-body col-md-7 pl-3");
+        titre.setAttribute("class", "card-title");
+        lientitre.setAttribute("class", "lien-article");
+        descr.setAttribute("class", "card-text card-description pl-2");
+        divright.setAttribute("class", "card-body col-md-2 pl-3");
+        titreprix.setAttribute("class", "card-title");
+        prix.setAttribute("class", "card-text font-weight-bold");
+        img.src= "./assets/images/"+ article.imageArticle;
+    
+        descr.textContent = article.descArticle;
+        lientitre.href = "./detail_article.html?id="+article.idArticle;
+        lientitre.textContent = article.prixArticle;
+        titreprix.textContent = "Prix :"
+        prix.textContent = article.prixArticle;
+        //on mets en forme tout le bousin
+        divleft.appendChild(img);
+        titre.appendChild(lientitre);
+        divmid.appendChild(titre);
+        divmid.appendChild(descr);
+        divright.appendChild(titreprix);
+        divright.appendChild(prix);
+        divcont.appendChild(divleft)
+        divcont.appendChild(divmid)
+        divcont.appendChild(divright)
+        card.appendChild(divcont);
+        //on affiche l'article à la suite des précédents
+        main.appendChild(card);
+        console.log(card);
     }
-)
-// var requestURL = '/assets/listearticle.json';
-// var requestURL = 'https://raw.githubusercontent.com/snozzy8z/ProjetGroupeLaMANU/J%C3%A9remy/assets/listearticle.json';
-// var requesturl = test;
-var truc= JSON.parse(test);
-console.log(truc);
-
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-    var listearticles = request.response;   
-   remplirliste(listearticles);
-  }
-//on se balade et on crée les cards necessaires
-  function remplirliste(articles){
-      for (const article of articles) {
-          console.log(article.idArticle);
-          //on crée l'architecture de la card article
-          var card = document.createElement("article");
-          card.setAttribute("class", "card m-2 col-12 border-0")
 
 
-      }
-  }
+});
 });
